ad37b2ae9409ebf6cb9c8520cd29a93dac000b7c
Sebastian Safari <ssebs@ymail.com>
2016-10-18 23:09:55 -0700

    Updated Android app. new icon, layout, and removed ip entry.

--- _Android/SaveStringServer/app/src/main/java/io/github/ssebs/savestringserver/MainActivity.java
+++ _Android/SaveStringServer/app/src/main/java/io/github/ssebs/savestringserver/MainActivity.java
@@ -1,14 +1,19 @@
 package io.github.ssebs.savestringserver;
 
+import android.net.Uri;
 import android.os.Handler;
 import android.os.Looper;
 import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.common.api.GoogleApiClient;
 
@@ -40,23 +45,46 @@ public class MainActivity extends AppCompatActivity {
 		// See https://g.co/AppIndexing/AndroidStudio for more information.
 		client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
+
+		EditText et = ((EditText) findViewById(R.id.txtToSend));
+
+		et.setOnKeyListener(new View.OnKeyListener()
+		{
+			public boolean onKey(View v, int keyCode, KeyEvent event)
+			{
+				if (event.getAction() == KeyEvent.ACTION_DOWN)
+				{
+					switch (keyCode)
+					{
+						case KeyEvent.KEYCODE_DPAD_CENTER:
+						case KeyEvent.KEYCODE_ENTER:
+							submit(v);
+							return true;
+						default:
+							break;
+					}
+				}
+				return false;
+			}
+		});
+
+
 	}
 
 
 	protected void submit(View v) {
-		serverIP = ((EditText) findViewById(R.id.server_ip)).getText().toString();
+//		serverIP = ((EditText) findViewById(R.id.ser)).getText().toString();
 		textToSend = ((EditText) findViewById(R.id.txtToSend)).getText().toString();
-
 		Thread init = new Thread(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					if (serverIP.length() < 1)
-						host = InetAddress.getByName("localhost");
-					else
-						//host = InetAddress.getByName(serverIP);
+//					if (serverIP.length() < 1)
+//						host = InetAddress.getByName("localhost");
+//					else
+//						//host = InetAddress.getByName(serverIP);
 
-						host = InetAddress.getByName("ssebs.ddns.net");
+					host = InetAddress.getByName("ssebs.ddns.net");
 
 					sock = new DatagramSocket();
 
@@ -122,5 +150,45 @@ public class MainActivity extends AppCompatActivity {
 		}
 
 
+	}
+
+	@Override
+	public void onStart() {
+		super.onStart();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		client.connect();
+		Action viewAction = Action.newAction(
+				Action.TYPE_VIEW, // TODO: choose an action type.
+				"Main Page", // TODO: Define a title for the content shown.
+				// TODO: If you have web page content that matches this app activity's content,
+				// make sure this auto-generated web page URL is correct.
+				// Otherwise, set the URL to null.
+				Uri.parse("http://host/path"),
+				// TODO: Make sure this auto-generated app URL is correct.
+				Uri.parse("android-app://io.github.ssebs.savestringserver/http/host/path")
+		);
+		AppIndex.AppIndexApi.start(client, viewAction);
+	}
+
+	@Override
+	public void onStop() {
+		super.onStop();
+
+		// ATTENTION: This was auto-generated to implement the App Indexing API.
+		// See https://g.co/AppIndexing/AndroidStudio for more information.
+		Action viewAction = Action.newAction(
+				Action.TYPE_VIEW, // TODO: choose an action type.
+				"Main Page", // TODO: Define a title for the content shown.
+				// TODO: If you have web page content that matches this app activity's content,
+				// make sure this auto-generated web page URL is correct.
+				// Otherwise, set the URL to null.
+				Uri.parse("http://host/path"),
+				// TODO: Make sure this auto-generated app URL is correct.
+				Uri.parse("android-app://io.github.ssebs.savestringserver/http/host/path")
+		);
+		AppIndex.AppIndexApi.end(client, viewAction);
+		client.disconnect();
 	}
 }
